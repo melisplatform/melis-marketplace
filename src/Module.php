0be6c793dda3728bf362a029880c5465ef1020ad
Ian Borla <benborla@icloud.com>
2017-10-11 19:22:04 +0800

    modified order of market packagist query

--- src/Module.php
+++ src/Module.php
@@ -30,6 +30,7 @@ class Module
             include __DIR__ . '/../config/app.interface.php',
             include __DIR__ . '/../config/app.tools.php',
             include __DIR__ . '/../config/app.forms.php',
+            include __DIR__ . '/../config/diagnostic.config.php',
         );
 
         foreach ($configFiles as $file) {
