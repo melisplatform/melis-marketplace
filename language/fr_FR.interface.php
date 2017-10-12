6bce11eda4df5de02e465e478da118dc782ae4ad
Ian Borla <benborla@icloud.com>
2017-10-12 15:22:02 +0800

    added fix on module that has no images

--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -21,6 +21,6 @@ return array(
     'tr_market_place_package_name' => ' Package name',
     'tr_market_place_package_installed' => ' Installed',
 
-
-
+    'tr_market_place_subtitle' => 'Cherchez de nouveau modules pour Melis platform et arretez de réinventer la roue !',
+    'tr_market_place_composer_instruction' => '<h5 class="widget-title">Vous voulez votre module listé sur cette page?</h5>Ajoutez cette ligne dans votre composer.json<pre class="meliscore-mp-composer-text"><code>"type": "melisplatform-module"</code></pre>et il sera automatiquement inclu dans les résultats de Packagist ! Les images et textes de présentation doivent être envoyés à : <a href="#">contact@melistechnology.com</a>',
 );
--- src/Module.php
+++ src/Module.php
@@ -61,12 +61,34 @@ class Module
 
         if (!empty($locale)){
 
-            $interfaceTransPath = 'module/MelisModuleConfig/language/MelisMarketPlace/' . $locale . '.interface.php';
-            $default = __DIR__ . '/../language/en_EN.interface.php';
-            $transPath = (file_exists($interfaceTransPath))? $interfaceTransPath : $default;
-            $translator->addTranslationFile('phparray', $transPath);
-        }
+            $translationType = array(
+                'interface',
+            );
 
+            $translationList = array();
+            if(file_exists($_SERVER['DOCUMENT_ROOT'].'/../module/MelisModuleConfig/config/translation.list.php')){
+                $translationList = include 'module/MelisModuleConfig/config/translation.list.php';
+            }
+
+            foreach($translationType as $type){
+
+                $transPath = '';
+                $moduleTrans = __NAMESPACE__."/$locale.$type.php";
+
+                if(in_array($moduleTrans, $translationList)){
+                    $transPath = "module/MelisModuleConfig/languages/".$moduleTrans;
+                }
+
+                if(empty($transPath)){
+
+                    // if translation is not found, use melis default translations
+                    $defaultLocale = (file_exists(__DIR__ . "/../language/$locale.$type.php"))? $locale : "en_EN";
+                    $transPath = __DIR__ . "/../language/$defaultLocale.$type.php";
+                }
+
+                $translator->addTranslationFile('phparray', $transPath);
+            }
+        }
     }
 
 }
