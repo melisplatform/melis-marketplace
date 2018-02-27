bd7da107502ccacdbe6e5a67b7711218b6e1870b
Ian Borla <benborla@icloud.com>
2018-02-27 12:05:51 +0800

    duplicate dbdeploy

--- src/Controller/MelisMarketPlaceController.php
+++ src/Controller/MelisMarketPlaceController.php
@@ -442,7 +442,7 @@ class MelisMarketPlaceController extends AbstractActionController
 
             if(!in_array($module, $activeModules)) {
                 // add to module loader
-                $defaultModules = array('MelisAssetManager','MelisComposerDeploy', 'MelisDbDeploy', 'MelisCore', 'MelisEngine', 'MelisFront', 'MelisDbDeploy');
+                $defaultModules = array('MelisAssetManager','MelisComposerDeploy', 'MelisDbDeploy', 'MelisCore', 'MelisEngine', 'MelisFront');
 
                 // remove MelisModuleConfig, to avoid duplication
                 $idx = array_search('MelisModuleConfig', $activeModules);
