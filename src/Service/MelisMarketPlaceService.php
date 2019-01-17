c394dbcd4cb8ce7ea29aac31edabf830e4ace8d0
benborla <benborla@icloud.com>
2019-01-17 16:07:31 +0800

    added validator method

--- src/Service/MelisMarketPlaceService.php
+++ src/Service/MelisMarketPlaceService.php
@@ -8,15 +8,138 @@
 
 namespace MelisMarketPlace\Service;
 
-
 use MelisCore\Service\MelisCoreGeneralService;
 
 class MelisMarketPlaceService extends MelisCoreGeneralService
 {
+    /** @var int */
     const NEED_UPDATE = -1;
+
+    /** @var int  */
     const UP_TO_DATE = 1;
+
+    /** @var int  */
     const IN_ADVANCE = 2;
+
+    /** @var string  */
     CONST DEV = 'dev-';
+
+    /** @var string  */
+    const MODULE_SETUP_CONTROLLER = 'MelisSetupController';
+
+    /** @var string */
+    const MODULE_SETUP_FORM = 'setupFormAction';
+
+    /** @var string  */
+    const MODULE_SETUP_VALIDATE_FORM = 'setupValidateDataAction';
+
+    /** @var string  */
+    const MODULE_SETUP_RESULT_FORM = 'setupResultAction';
+
+    /** @var string  */
+    const MODULE_SETUP_FORM_SHOW_ON_DOWNLOAD = 'displayFormOnMarketPlaceDownload';
+
+    /** @var string  */
+    const MODULE_SETUP_FORM_SHOW_ON_UPDATE = 'displayFormOnMarketPlaceUpdate';
+
+    /** @var string  */
+    const ACTION_DOWNLOAD = 'download';
+
+    /** @var string  */
+    const ACTION_UPDATE = 'update';
+
+    /**
+     * Get the value of the class' property
+     * @param $class
+     * @param $prop
+     *
+     * @return mixed|null
+     * @throws \ReflectionException
+     */
+    protected function getClassProperty($class, $prop)
+    {
+        if (class_exists($class)) {
+            $reflection = new \ReflectionClass($class);
+            $property = $reflection->getProperty($prop)->getValue(new $class);
+            return $property;
+        }
+
+        return null;
+    }
+
+    /**
+     * Flag for Marketplace whether to display the setup form or not when downloading
+     * @param $module
+     *
+     * @return bool|mixed|null
+     * @throws \ReflectionException
+     */
+    protected function showSetupFormOnDownload($module)
+    {
+        $moduleClass  = implode('\\', [$module, 'Controller', self::MODULE_SETUP_CONTROLLER]);
+
+        if (class_exists($moduleClass)) {
+            return $this->getClassProperty($moduleClass, self::MODULE_SETUP_FORM_SHOW_ON_DOWNLOAD);
+        }
+
+        return false;
+    }
+
+    /**
+     * Flag for Marketplace whether to display the setup form or not when updating
+     * @param $module
+     *
+     * @return bool|mixed|null
+     * @throws \ReflectionException
+     */
+    protected function showSetupFormOnUpdate($module)
+    {
+        $moduleClass  = implode('\\', [$module, 'Controller', self::MODULE_SETUP_CONTROLLER]);
+
+        if (class_exists($moduleClass)) {
+            return $this->getClassProperty($moduleClass, self::MODULE_SETUP_FORM_SHOW_ON_UPDATE);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $module
+     *
+     * @return null
+     */
+    public function getFormDom($module)
+    {
+        $class   = implode('\\', [$module, 'Controller', str_replace('Controller', '',self::MODULE_SETUP_CONTROLLER)]);
+        $forward = $this->getServiceLocator()->get('Application')->getMvcEvent()->getTarget()->forward();
+        $form  = $forward->dispatch($class, ['action' => str_replace('Action', '', self::MODULE_SETUP_FORM)]);
+
+        /** @var \Zend\View\Renderer\RendererInterface $renderer */
+        $renderer = $this->getServiceLocator()->get('Zend\View\Renderer\RendererInterface');
+        $formDom = (new \Zend\Mime\Part($renderer->render($form)))->getContent() ?: null;
+
+        return $formDom;
+    }
+
+    /**
+     * @param $module
+     * @param $action
+     *
+     * @return bool
+     * @throws \ReflectionException
+     */
+    public function showForm($module, $action)
+    {
+        if ($action === self::ACTION_DOWNLOAD && $this->showSetupFormOnDownload($module)) {
+            return true;
+        }
+
+        if ($action === self::ACTION_UPDATE && $this->showSetupFormOnUpdate($module)) {
+            return true;
+        }
+
+        return false;
+    }
 
     /**
      * @description Function to get the local version of a module
