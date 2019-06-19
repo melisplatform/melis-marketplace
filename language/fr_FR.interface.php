4c9321dbb01d0049aa797e7c70aa56e474e91a29
Salcover <simonalcover@gmail.com>
2019-06-19 16:12:22 +0800

    Ticket #: 3914 (marketplace - Melis Demo CMS setup translation missing)

--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -82,9 +82,9 @@ return array(
 
     'tr_melis_marketplace_common_no_skip' => 'No, skip',
 
-    'tr_melis_market_place_setup_title' => 'Setup for %s',
+    'tr_melis_market_place_setup_title' => 'Configuration %s',
     'tr_melis_market_place_has_setup_form' => 'An additional setup has been found, would you like to set up %s?',
-    'tr_melis_marketplace_setup_module_modal_title' => 'Setup for %s',
+    'tr_melis_marketplace_setup_module_modal_title' => 'Configuration %s',
     'tr_melis_marketplace_setup_error' => 'Something went wrong with processing the additional setup, please reload the page',
 
     'tr_marketplace_common_submit' => 'Enregistrer',
