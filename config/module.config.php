6c7269a2ab7fdfa24ae8c4c056b36934908acaa7
Ralph Bryan Brioso <bbrioso@melistechnology.com>
2020-03-30 11:53:35 +0800

    Exception and Not found reason removed

--- config/module.config.php
+++ config/module.config.php
@@ -101,12 +101,6 @@ return [
         ],
     ],
     'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'template_map' => [
-
-        ],
         'template_path_stack' => [
             __DIR__ . '/../view',
         ],
