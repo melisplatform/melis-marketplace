cce3140a5f9ba821bdcc2c22073a90c861cf250e
Ralph Bryan Brioso <bbrioso@melistechnology.com>
2020-04-17 16:54:10 +0800

    Listeners updates

--- src/Listener/MelisMarketPlaceTestListener.php
+++ src/Listener/MelisMarketPlaceTestListener.php
@@ -9,25 +9,24 @@
 
 namespace MelisMarketPlace\Listener;
 
-use Laminas\EventManager\AbstractListenerAggregate;
+use MelisCore\Listener\MelisGeneralListener;
 use Laminas\EventManager\EventManagerInterface;
 use Laminas\EventManager\ListenerAggregateInterface;
 
-class MelisMarketPlaceTestListener extends AbstractListenerAggregate implements ListenerAggregateInterface
+class MelisMarketPlaceTestListener extends MelisGeneralListener implements ListenerAggregateInterface
 {
     public function attach(EventManagerInterface $events, $priority = 1)
     {
-        $sharedEvents = $events->getSharedManager();
-
-        $callBackHandler = $sharedEvents->attach(
-            '*', 'melis_marketplace_site_intall_test',
+        $this->attachEventListener(
+            $events,
+            '*',
+            'melis_marketplace_site_intall_test',
             function ($e) {
                 $params = $params = $e->getParams();
                 $logPath = $_SERVER['DOCUMENT_ROOT'] . '/../cache/marketplace.log';
                 file_put_contents($logPath, print_r($params, true) . PHP_EOL . PHP_EOL, FILE_APPEND);
             },
-            -10000);
-
-        $this->listeners[] = $callBackHandler;
+            -10000
+        );
     }
 }
