8b040f664048a4c6938a7cfe2e4fc726fab9ea56
unknown <crismakabenta@gmail.com>
2020-06-05 12:29:34 +0800

    Removed saving 404 in marketplace, transferred to download.config.php of the site

--- src/Service/MelisMarketPlaceSiteService.php
+++ src/Service/MelisMarketPlaceSiteService.php
@@ -116,7 +116,6 @@ class MelisMarketPlaceSiteService extends MelisCoreGeneralService
 
             $siteId = $this->getCurrentPageId();
             $platformName = $this->getPlatform()->plf_name;
-            $page404Id = (int)$siteId + 36;
 
             $siteTable = $this->siteTable()->save([
                 'site_name' => $module,
@@ -146,13 +145,10 @@ class MelisMarketPlaceSiteService extends MelisCoreGeneralService
                 'slang_site_id' => $siteTable,
                 'slang_lang_id' => 1,
             ]);
+
             /**
-             * Save the site 404 page od
+             * Page 404 saving is done in the download.config.php
              */
-            $this->site404Table()->save([
-                's404_site_id' => $siteTable,
-                's404_page_id' => $page404Id,
-            ]);
         } else {
             throw new EmptySiteException('Site data is empty', 500);
         }
