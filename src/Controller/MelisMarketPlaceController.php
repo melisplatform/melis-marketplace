81a5f1d23cc9ce51eccf672d4708369d112057f7
Maria Teresa Pomar <mpomar@melistechnology.com>
2022-05-16 11:26:00 +0800

    removed session_write_close func

--- src/Controller/MelisMarketPlaceController.php
+++ src/Controller/MelisMarketPlaceController.php
@@ -556,7 +556,6 @@ class MelisMarketPlaceController extends MelisAbstractActionController
      */
     public function melisMarketPlaceProductDoAction()
     {   
-        session_write_close();
         set_time_limit(0);
         ini_set('memory_limit', '-1');
                 
